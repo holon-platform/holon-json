04665bbf81923353012c7f181ee022e379815a3b
Riccardo Righi <rr9379@gmail.com>
2018-04-05 11:28:10 +0200

    Make SpringJacksonConfiguration an interface

--- jackson-spring/src/main/java/com/holonplatform/json/jackson/spring/SpringJacksonConfiguration.java
+++ jackson-spring/src/main/java/com/holonplatform/json/jackson/spring/SpringJacksonConfiguration.java
@@ -15,56 +15,33 @@
  */
 package com.holonplatform.json.jackson.spring;
 
-import java.io.Serializable;
-import java.util.Optional;
-
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
+import com.holonplatform.core.internal.utils.ObjectUtils;
 import com.holonplatform.core.property.PropertyBox;
 import com.holonplatform.json.jackson.JacksonConfiguration;
+import com.holonplatform.json.jackson.spring.internal.JacksonRestTemplateUtils;
 
 /**
- * Utility class to handle Jackson configuration for Spring.
+ * Utility interface to handle Jackson configuration in Spring environment.
  *
  * @since 5.0.0
  */
-public final class SpringJacksonConfiguration implements Serializable {
-
-	private static final long serialVersionUID = 1337470063296589625L;
-
-	private SpringJacksonConfiguration() {
-	}
+public interface SpringJacksonConfiguration {
 
 	/**
 	 * Configure Spring RestTemplate, setting up serializers and deserializers for {@link PropertyBox} type handling in
 	 * Jackson HttpMessageConverters, if any. If no Jackson HttpMessageConverter is registered, a configured
 	 * {@link MappingJackson2HttpMessageConverter} will be registered in RestTemplate.
-	 * <p>
-	 * In order to this method to work, <code>spring-web</code> artifact must be present in classpath.
-	 * </p>
-	 * @param restTemplate RestTemplate to configure
+	 * @param restTemplate RestTemplate to configure (not null)
+	 * @return The configured RestTemplate instance
 	 */
-	public static void configure(RestTemplate restTemplate) {
-		JacksonConfiguration.configure(
-				getJacksonConverter(restTemplate).orElse(new MappingJackson2HttpMessageConverter()).getObjectMapper());
-
-	}
-
-	/**
-	 * Get a registered AbstractJackson2HttpMessageConverter from RestTemplate
-	 * @param restTemplate RestTemplate
-	 * @return Optional AbstractJackson2HttpMessageConverter, empty if not registered
-	 */
-	private static Optional<AbstractJackson2HttpMessageConverter> getJacksonConverter(RestTemplate restTemplate) {
-		for (HttpMessageConverter<?> converter : restTemplate.getMessageConverters()) {
-			if (AbstractJackson2HttpMessageConverter.class.isAssignableFrom(converter.getClass())) {
-				return Optional.of((AbstractJackson2HttpMessageConverter) converter);
-			}
-		}
-		return Optional.empty();
+	public static RestTemplate configure(RestTemplate restTemplate) {
+		ObjectUtils.argumentNotNull(restTemplate, "RestTemplate must be not null");
+		JacksonConfiguration.configure(JacksonRestTemplateUtils.getJacksonConverter(restTemplate)
+				.orElse(new MappingJackson2HttpMessageConverter()).getObjectMapper());
+		return restTemplate;
 	}
 
 }
